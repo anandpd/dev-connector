5f2be4f3d7718ef8a0234788224e76b96e70fc78
anandpd <danand407@gmail.com>
2020-07-24 15:50:09 +0530

    ReactRouter

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,25 @@
 import React, { Fragment } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import NavBar from "./Components/NavBar";
 import Landing from "./Components/Landing";
+import Register from "./Components/auth/Register";
+import Login from "./Components/auth/Login";
+
 import "./App.css";
 
 const App = () => (
-  <Fragment>
-        <NavBar />
-        <Landing />
-  </Fragment>
+  <Router>
+    <Fragment>
+      <NavBar />
+      <Route exact path='/' component={Landing} />
+      <section className='container'>
+        <switch>
+          <Route exact path='/register' component={Register} />
+          <Route exact path='/login' component={Login} />
+        </switch>
+      </section>
+    </Fragment>
+  </Router>
 );
 
 export default App;
