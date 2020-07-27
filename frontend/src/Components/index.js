db98ab7052074a6d6203e007bea623ef9bc51234
Anand <ananddeep818@gmail.com>
2020-07-27 21:14:14 +0530

    frontendnew

--- frontend/src/Components/index.js
+++
@@ -1,4 +0,0 @@
-export { default as Navbar } from "./NavBar";
-export { default as Landing } from "./Landing";
-export { default as Login } from "./auth/Login";
-export { default as Register } from "./auth/Register";
